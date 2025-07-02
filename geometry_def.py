mesh computation fix (it was double number of nodes on the cylinder

geometry_def.py
@@ -186,7 +186,7 @@ class Circle:
         self.dim = 1
 
         # create a structured arcCricle to merge in one curveloop
-        self.distribution = int(n_points*2.*np.pi)
+        self.distribution = int(n_points*np.pi)
         self.arcCircle_list = [
             gmsh.model.occ.addCircle(
                 self.xc,